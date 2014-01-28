08ad759bf637699d54a4fcdb175b6cdc458c314f
Eric Turgeon <ericbsd@ericbsd.com>
2014-01-27 22:16:05 -0400

    Initiall commit

---
+++ updatemgr/mainWindow.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+#
+
+import gtk
+import os
+import os.path
+from subprocess import Popen, PIPE, STDOUT, call
+
+
+class Entire():
+
+    def close_application(self, widget):
+        gtk.main_quit()
+
+    def __init__(self):
+        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        window.connect("destroy", self.close_application)
+        window.set_size_request(700, 500)
+        window.set_resizable(False)
+        window.set_title("Update Manager")
+        window.set_border_width(0)
+        window.set_position(gtk.WIN_POS_CENTER)
+        window.set_icon_from_file("/usr/local/etc/gbi/logo.png")
+        box1 = gtk.VBox(False, 0)
+        window.add(box1)
+        box1.show()
+        box2 = gtk.VBox(False, 10)
+        box2.set_border_width(20)
+        box1.pack_start(box2, True, True, 0)
+        box2.show()
+        # Title
+        titleText = "System updates available!"
+        Title = gtk.Label("<b><span size='large'>%s</span></b>" % titleText)
+        Title.set_use_markup(True)
+        box2.pack_start(Title, False, False, 0)
+        # chose Disk
+
+        sw = gtk.ScrolledWindow()
+        sw.set_shadow_type(gtk.SHADOW_ETCHED_IN)
+        sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        store = gtk.TreeStore(str, 'gboolean')
+        #for disk in disk_query():
+        store.append(None, ["FreeBSD 10.0-p1 update", True])
+        treeView = gtk.TreeView(store)
+        treeView.set_model(store)
+        treeView.set_rules_hint(True)
+        cell = gtk.CellRendererText()
+        column = gtk.TreeViewColumn(None, cell, text=0)
+        column_header = gtk.Label('Update')
+        column_header.set_use_markup(True)
+        column_header.show()
+        column.set_widget(column_header)
+        column.set_sort_column_id(0)
+        #cell2 = gtk.CellRendererText()
+        #column2 = gtk.TreeViewColumn(None, cell2, text=0)
+        #column_header2 = gtk.Label('Size(MB)')
+        #column_header2.set_use_markup(True)
+        #column_header2.show()
+        #column2.set_widget(column_header2)
+        #cell3 = gtk.CellRendererText()
+        #column3 = gtk.TreeViewColumn(None, cell3, text=0)
+        #column_header3 = gtk.Label('Scheme')
+        #column_header3.set_use_markup(True)
+        #column_header3.show()
+        #column3.set_widget(column_header3)
+        column.set_attributes(cell, text=0)
+        #column2.set_attributes(cell2, text=1)
+        #column3.set_attributes(cell3, text=2)
+        treeView.append_column(column)
+        treeView.set_headers_visible(False)
+        #treeView.append_column(column2)
+        #treeView.append_column(column3)
+        tree_selection = treeView.get_selection()
+        tree_selection.set_mode(gtk.SELECTION_SINGLE)
+        #tree_selection.connect("changed", self.Selection_Variant)
+        sw.add(treeView)
+        sw.show()
+        box2.pack_start(sw, True, True, 10)
+        box2 = gtk.HBox(False, 10)
+        box2.set_border_width(5)
+        box1.pack_start(box2, False, False, 0)
+        box2.show()
+        # Add button
+        #box2.pack_start(use_disk_bbox(True,
+                        #10, gtk.BUTTONBOX_END),
+                        #True, True, 5)
+        window.show_all()
+
+Entire()
+gtk.main()
---
+++ updatemgr/trayIcon.py
@@ -0,0 +1,117 @@
+#!/usr/local/bin/python
+
+import gtk
+import gobject
+#from subprocess import call
+
+
+class TrayIcon(object):
+    def __init__(self):
+        self.statusIcon = gtk.StatusIcon()
+        self.statusIcon.set_tooltip('System Update availeble')
+        self.statusIcon.set_visible(True)
+
+        self.menu = gtk.Menu()
+
+        self.menu.show_all()
+        self.act = False
+        self.statusIcon.connect("activate", self.leftclick)
+        self.statusIcon.connect('popup-menu', self.icon_clicked)
+
+    #def nm_menu(self):
+        #self.ssid_name = ssidliste()
+        #self.menu = gtk.Menu()
+        #e_title = gtk.MenuItem()
+        #e_title.set_label("Ethernet Network")
+        #e_title.set_sensitive(False)
+        #self.menu.append(e_title)
+        #disconnected = gtk.MenuItem()
+        #if wiredonlineinfo() is True:
+            #wired_item = gtk.MenuItem("Wired Connection")
+            #self.menu.append(wired_item)
+            #disconnect_item = gtk.ImageMenuItem("Disconnect")
+            #disconnect_item.connect("activate", self.wireddisconnect)
+            #self.menu.append(disconnect_item)
+        #elif wiredconnectedinfo() is True:
+            #disconnected.set_label("disconnected")
+            #disconnected.set_sensitive(False)
+            #self.menu.append(disconnected)
+            #wired_item = gtk.MenuItem("Wired Connection")
+            #wired_item.connect("activate", self.wiredconnect)
+            #self.menu.append(wired_item)
+        #else:
+            #disconnected.set_label("disconnected")
+            #disconnected.set_sensitive(False)
+            #self.menu.append(disconnected)
+        #self.menu.append(gtk.SeparatorMenuItem())
+        #w_title = gtk.MenuItem()
+        #w_title.set_label("Wi-Fi Network")
+        #w_title.set_sensitive(False)
+        #self.menu.append(w_title)
+        #if get_ssid() is None:
+            #pass
+        #else:
+            #bar = barpercent(get_ssid())
+            #connection_item = gtk.ImageMenuItem(get_ssid())
+            #connection_item.set_image(self.openwifi(bar))
+            #connection_item.show()
+            #disconnect_item = gtk.MenuItem("Disconnect")
+            #disconnect_item.connect("activate", self.disconnectfromwifi)
+            #self.menu.append(connection_item)
+            #self.menu.append(disconnect_item)
+            #self.menu.append(gtk.SeparatorMenuItem())
+        #for name in self.ssid_name:
+            #bar = barpercent(name)
+            #if get_ssid() == name:
+                #pass
+            #else:
+                #menu_item = gtk.ImageMenuItem(name)
+                #menu_item.set_image(self.openwifi(bar))
+                #if keyinfo(name) == 'EP':
+                    #menu_item.connect("activate", self.menu_click_look, name)
+                #elif keyinfo(name) == 'E':
+                    #menu_item.connect("activate", self.menu_click_open, name)
+                #menu_item.show()
+                #self.menu.append(menu_item)
+        #self.menu.show_all()
+        #return self.menu
+
+    def leftclick(self, status_icon):
+        button = 1
+        position = gtk.status_icon_position_menu
+        time = gtk.get_current_event_time()
+        self.nm_menu()
+        self.menu.popup(None, None, position, button, time, status_icon)
+
+    def icon_clicked(self, status_icon, button, time):
+        position = gtk.status_icon_position_menu
+        self.nm_menu()
+        self.menu.popup(None, None, position, button, time, status_icon)
+
+    def openwifi(self, bar):
+        img = gtk.Image()
+        if bar > 75:
+            img.set_from_file(sgnal100)
+        elif bar > 50:
+            img.set_from_file(sgnal75)
+        elif bar > 25:
+            img.set_from_file(sgnal50)
+        elif bar > 5:
+            img.set_from_file(sgnal25)
+        else:
+            img.set_from_file(sgnal0)
+        img.show()
+        return img
+
+    def check(self):
+        self.statusIcon.set_from_stock(gtk.STOCK_DIALOG_WARNING)
+        return True
+
+    def tray(self):
+        self.check()
+        gobject.timeout_add(10000, self.check)
+        gtk.main()
+
+
+i = TrayIcon()
+i.tray()
