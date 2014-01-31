193dbc47b3fa077b2176766ab187068dcb69f6c6
ericbsd <ericturgeon.bsd@gmail.com>
2014-01-30 22:05:56 -0400

    Update Manager layout

--- updatemgr/mainWindow.py
+++ updatemgr/mainWindow.py
@@ -1,17 +1,31 @@
 #!/usr/local/bin/python
 
 import gtk
+import gobject
 from updateHandler import lookUpdate
 
-class Entire():
+
+class GUI_Controller:
 
     def close_application(self, widget):
         gtk.main_quit()
 
+    def create_bbox(self, horizontal, spacing, layout):
+        bbox = gtk.HButtonBox()
+        bbox.set_border_width(10)
+        bbox.set_spacing(10)
+        button = gtk.Button(stock=gtk.STOCK_PREFERENCES)
+        #button.connect("clicked", root_window)
+        bbox.add(button)
+        button = gtk.Button(stock=gtk.STOCK_CLOSE)
+        bbox.add(button)
+        button.connect("clicked", self.close_application)
+        return bbox
+
     def __init__(self):
         window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         window.connect("destroy", self.close_application)
-        window.set_size_request(700, 500)
+        window.set_size_request(700, 550)
         window.set_resizable(False)
         window.set_title("Update Manager")
         window.set_border_width(0)
@@ -20,7 +34,8 @@ class Entire():
         box1 = gtk.VBox(False, 0)
         window.add(box1)
         box1.show()
-        box2 = gtk.VBox(False, 10)
+        #box1.set_border_width(20)
+        box2 = gtk.VBox(False, 0)
         box2.set_border_width(20)
         box1.pack_start(box2, True, True, 0)
         box2.show()
@@ -29,58 +44,81 @@ class Entire():
         Title = gtk.Label("<b><span size='large'>%s</span></b>" % titleText)
         Title.set_use_markup(True)
         box2.pack_start(Title, False, False, 0)
-        # chose Disk
-
+        self.mdl = Store.get_model()
+        self.view = Display.make_view(self.mdl)
         sw = gtk.ScrolledWindow()
         sw.set_shadow_type(gtk.SHADOW_ETCHED_IN)
         sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-        store = gtk.TreeStore(str, 'gboolean')
-        #for disk in disk_query():
-        store.append(None, [lookUpdate(), True])
-        treeView = gtk.TreeView(store)
-        treeView.set_model(store)
-        treeView.set_rules_hint(True)
-        cell = gtk.CellRendererText()
-        column = gtk.TreeViewColumn(None, cell, text=0)
-        column_header = gtk.Label('Update')
-        column_header.set_use_markup(True)
-        column_header.show()
-        column.set_widget(column_header)
-        column.set_sort_column_id(0)
-        #cell2 = gtk.CellRendererText()
-        #column2 = gtk.TreeViewColumn(None, cell2, text=0)
-        #column_header2 = gtk.Label('Size(MB)')
-        #column_header2.set_use_markup(True)
-        #column_header2.show()
-        #column2.set_widget(column_header2)
-        #cell3 = gtk.CellRendererText()
-        #column3 = gtk.TreeViewColumn(None, cell3, text=0)
-        #column_header3 = gtk.Label('Scheme')
-        #column_header3.set_use_markup(True)
-        #column_header3.show()
-        #column3.set_widget(column_header3)
-        column.set_attributes(cell, text=0)
-        #column2.set_attributes(cell2, text=1)
-        #column3.set_attributes(cell3, text=2)
-        treeView.append_column(column)
-        treeView.set_headers_visible(False)
-        #treeView.append_column(column2)
-        #treeView.append_column(column3)
-        tree_selection = treeView.get_selection()
-        tree_selection.set_mode(gtk.SELECTION_SINGLE)
-        #tree_selection.connect("changed", self.Selection_Variant)
-        sw.add(treeView)
+        sw.add(self.view)
         sw.show()
+        box2.pack_start(sw, True, True, 10)
+        box2.pack_start(self.create_bbox(True,
+        10, gtk.BUTTONBOX_END), False, False, 5)
+        sw = gtk.ScrolledWindow()
+        sw.set_shadow_type(gtk.SHADOW_ETCHED_IN)
+        sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        textview = gtk.TextView()
+        textbuffer = textview.get_buffer()
+        sw.add(textview)
+        sw.show()
+        textview.show()
+        textview.set_editable(False)
+        textview.set_cursor_visible(False)
+        infile = open("mainWindow.py", "r")
+        if infile:
+            string = infile.read()
+            infile.close()
+            textbuffer.set_text(string)
         box2.pack_start(sw, True, True, 10)
         box2 = gtk.HBox(False, 10)
         box2.set_border_width(5)
         box1.pack_start(box2, False, False, 0)
+        #box1.set_border_width(0)
         box2.show()
         # Add button
-        #box2.pack_start(use_disk_bbox(True,
-                        #10, gtk.BUTTONBOX_END),
-                        #True, True, 5)
+        box2.pack_start(self.create_bbox(True,
+        10, gtk.BUTTONBOX_END), True, True, 5)
         window.show_all()
 
-Entire()
+
+class InfoModel:
+
+    def __init__(self):
+        self.tree_store = gtk.TreeStore(gobject.TYPE_STRING,
+        gobject.TYPE_BOOLEAN)
+
+        self.tree_store.append(None, (lookUpdate(), True))
+        return
+
+    def get_model(self):
+        if self.tree_store:
+            return self.tree_store
+        else:
+            return None
+
+
+class DisplayModel:
+
+    def make_view(self, model):
+        self.view = gtk.TreeView(model)
+        self.renderer = gtk.CellRendererText()
+        self.renderer1 = gtk.CellRendererToggle()
+        self.renderer1.set_property('activatable', True)
+        self.renderer1.connect('toggled', self.col1_toggled_cb, model)
+        self.column0 = gtk.TreeViewColumn("Name", self.renderer, text=0)
+        self.column1 = gtk.TreeViewColumn("Complete", self.renderer1)
+        self.column1.add_attribute(self.renderer1, "active", 1)
+        self.view.append_column(self.column1)
+        self.view.append_column(self.column0)
+        self.view.set_headers_visible(False)
+        return self.view
+
+    def col1_toggled_cb(self, cell, path, model):
+        model[path][1] = not model[path][1]
+        print(("Toggle '%s' to: %s" % (model[path][0], model[path][1],)))
+        return
+
+Store = InfoModel()
+Display = DisplayModel()
+GUI_Controller()
 gtk.main()
--- updatemgr/updateHandler.py
+++ updatemgr/updateHandler.py
@@ -5,22 +5,34 @@ from subprocess import Popen, PIPE, STDOUT, call
 fetch = "freebsd-update cron"
 fbtag = '/var/db/freebsd-update/tag'
 updatemgrtag = '/var/db/updatemgr/tag'
+fbvcmd = "freebsd-version"
+installfbupdate = "freebsd-update install"
+
+
+def chmodfu():
+    call('chmod 755 /var/db/freebsd-update', shell=True)
 
 
 def checkUpdate():
     if os.path.exists(fbtag):
+        call('chmod 755 /var/db/freebsd-update', shell=True)
         uptag = open(fbtag, 'r')
-        taglist = uptag.readlines()[0].rstrip().split('|')
-        print(taglist)
-        return True
-    else:
-        return False
+        tag = uptag.readlines()[0].rstrip().split('|')
+        upversion = tag[2] + "-p" + tag[3]
+        fbv = Popen(fbvcmd, shell=True, stdin=PIPE, stdout=PIPE,
+        stderr=STDOUT, close_fds=True)
+        fbsdversion = fbv.stdout.readlines()[0].rstrip()
+        if fbsdversion == upversion:
+            return True
+        else:
+            return False
 
 
 def lookUpdate():
     if os.path.exists(fbtag):
+        call('chmod 755 /var/db/freebsd-update', shell=True)
         uptag = open(fbtag, 'r')
         tag = uptag.readlines()[0].rstrip().split('|')
-        return "FreeBSD " + tag[2] + " p" + tag[3] + " system update"
+        return "FreeBSD " + tag[2] + "-p" + tag[3] + " system update"
 
-print lookUpdate()
+print(lookUpdate())
