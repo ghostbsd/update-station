a3b48729a9f5e4cad2886df09ed2c24f64d37a90
Eric Trugeon <ericbsd@protonmail.com>
2020-02-01 13:17:20 -0400

    rework updateHandler.py and deprecated fbsdpkgupdate
    renamed funcitons in updateHandler.py
    update the display of packages in update manager window

--- src/updateHandler.py
+++ src/updateHandler.py
@@ -1,128 +1,129 @@
 #!/usr/local/bin/python
-
 """All fuction to handle various update for GhostBSD."""
 
-from os import listdir, path
-from subprocess import Popen, PIPE, STDOUT, call
-# import urllib
-import platform
+from subprocess import Popen, PIPE, call
 
-ustation_db = '/var/db/update-station/'
-pkglockfile = '%slock-pkgs' % ustation_db
-pkglist = '/var/db/pkg-update-check/pkg-update-list'
-fbsduf = '/var/db/freebsd-update-check/'
-pkgupdatefile = ustation_db + 'pkg-to-upgrade'
-fbtag = '%stag' % fbsduf
-fblist = '%stag' % fbsduf
-fbvcmd = "freebsd-version"
-fblist = '%stag' % fbsduf
-arch = platform.uname()[4]
-checkfbsdupdate = 'fbsdupdatecheck check'
-fetchfbsdupdate = 'fbsdupdatecheck fetch'
-installfbsdupdate = 'fbsdupdatecheck install'
-checkpkgupgrade = 'fbsdpkgupdate check'
-fetchpkgupgrade = 'pkg upgrade -Fy'
-isntallpkgupgrade = 'pkg upgrade -y'
-lockpkg = 'pkg lock -y '
-unlockallpkg = 'pkg unlock -ay'
-unlockpkg = 'pkg unlock -y '
-
-release = Popen('uname -r', shell=True, stdin=PIPE, stdout=PIPE,
-                stderr=STDOUT, close_fds=True
-                ).stdout.readlines()[0].rstrip()
-
-if not path.isdir(ustation_db):
-    Popen('mkdir -p ' + ustation_db, shell=True, close_fds=True)
-    Popen('chmod -R 665 ' + ustation_db, shell=True, close_fds=True)
-
-fbftp = "ftp://ftp.freebsd.org/pub/"
-fbsrcurl = fbftp + "FreeBSD/releases/%s/%s/%s/src.txz" % (arch, arch, release)
-cleandotdesktop = "sh /usr/local/lib/update-station/cleandesktop.sh"
+ustation_db = '/var/db/update-station'
+pkg_lock_file = f'{ustation_db}/lock-pkgs'
 
 
-def listofinstal():
-    ls = listdir(fbsduf)
-    for line in ls:
-        if 'install.' in line:
-            uptag = open(fbsduf + line + '/INDEX-NEW', 'r')
-            info = uptag.readlines()
-            return info
+def get_pkg_upgrade():
+    get_pkg_upgrade = 'pkg install -nf vscode'
+    pkg_upgrade = Popen(
+        get_pkg_upgrade,
+        shell=True,
+        stdout=PIPE,
+        close_fds=True,
+        universal_newlines=True,
+        encoding='utf-8'
+    )
+    upgrade_verbose = pkg_upgrade.stdout.read()
+    return upgrade_verbose
 
 
-def lookfbsdupdate():
-    if path.exists(fbtag):
-        uptag = open(fbtag, 'r')
-        tag = uptag.readlines()[0].rstrip().split('|')
-        return "FreeBSD Update: " + tag[2] + "-p" + tag[3]
-    else:
-        return None
+def get_pkg_upgrade_data():
+    update_pkg = get_pkg_upgrade()
+    update_pkg_list = update_pkg.splitlines()
+    pkg_to_remove = []
+    pkg_to_upgrade = []
+    pkg_to_install = []
+    pkg_to_reinstall = []
+    stop = False
+    if 'REMOVED:' in update_pkg:
+        for line in update_pkg_list:
+            if 'REMOVED:' in line:
+                stop = True
+            elif stop is True and line == '':
+                stop = False
+                break
+            elif stop is True:
+                pkg_to_remove.append(line.strip())
+    if 'UPGRADED:' in update_pkg:
+        for line in update_pkg_list:
+            if 'UPGRADED:' in line:
+                stop = True
+            elif stop is True and line == '':
+                stop = False
+                break
+            elif stop is True:
+                pkg_to_upgrade.append(line.strip())
+    if ' INSTALLED:' in update_pkg:
+        for line in update_pkg_list:
+            if ' INSTALLED:' in line:
+                stop = True
+            elif stop is True and line == '':
+                stop = False
+                break
+            elif stop is True:
+                pkg_to_install.append(line.strip())
+    if 'REINSTALLED:' in update_pkg:
+        for line in update_pkg_list:
+            if 'REINSTALLED:' in line:
+                stop = True
+            elif stop is True and line == '':
+                stop = False
+                break
+            elif stop is True:
+                pkg_to_reinstall.append(line.strip())
+    pkg_dictionaire = {
+        'remove': pkg_to_remove,
+        'upgrade': pkg_to_upgrade,
+        'install': pkg_to_install,
+        'reinstall': pkg_to_reinstall
+    }
+    return pkg_dictionaire
 
 
-def updatetext():
-    udatetitle = lookfbsdupdate()
-    text = "Update Details:\n"
-    text += "%s\n\n" % udatetitle
-    text += "The following files will be update:\n"
-    for line in listofinstal():
-        txtlist = line.split('|')
-        text += "%s" % txtlist[0] + "\n"
-    return text
-
-
-def checkpkgupdate():
-    call(checkpkgupgrade, shell=True, stdout=PIPE, close_fds=True,
-         universal_newlines=True)
+def lock_pkg(Lock_pkg_list):
+    for line in Lock_pkg_list:
+        call(
+            f'pkg lock -y {line.strip()}',
+            shell=True
+        )
     return True
 
 
-def runcheckupdate():
-    checkpkgupdate()
+def unlock_all_pkg():
+    call(
+        'pkg unlock -ay',
+        shell=True
+    )
     return True
 
 
-def checkpkgupdatefromfile():
-    uptag = open(pkglist, 'r')
-    if "UPGRADED:" in uptag.read():
-        return True
-    else:
-        return False
-
-
-def pkgupdatelist():
-    uppkg = open(pkglist, 'r')
-    pkgarr = []
-    for line in uppkg.readlines():
-        if "->" in line:
-            pkgarr.append(line.rstrip()[1:])
-    return pkgarr
-
-
-def lockpkg(pkglist):
-    for line in pkglist:
-        call(pkglist + line.rstrip(), shell=True)
+def unlock_pkg(Lock_pkg_list):
+    for line in Lock_pkg_list:
+        call(
+            f'pkg unlock -y {line.strip()}',
+            shell=True
+        )
     return True
 
 
-def unlockpkg():
-    # unlock all pkg
-    call(unlockpkg, shell=True)
-    return True
-
-
-def fetchpkgupdate():
-    fetch = Popen(fetchpkgupgrade, shell=True, stdout=PIPE, close_fds=True,
-                  universal_newlines=True)
+def fetch_pkg_update():
+    fetch = Popen(
+        'pkg upgrade -Fy',
+        shell=True,
+        stdout=PIPE,
+        close_fds=True,
+        universal_newlines=True
+    )
     return fetch.stdout
 
 
-def installpkgupdate():
-    install = Popen(isntallpkgupgrade, shell=True, stdout=PIPE, close_fds=True,
-                    universal_newlines=True)
+def install_pkg_update():
+    install = Popen(
+        'pkg upgrade -y',
+        shell=True,
+        stdout=PIPE,
+        close_fds=True,
+        universal_newlines=True
+    )
     return install.stdout
 
 
-def checkforupdate():
-    if checkpkgupdatefromfile() is True:
-        return True
-    else:
+def check_for_update():
+    if 'Your packages are up to date' in get_pkg_upgrade():
         return False
+    else:
+        return True
