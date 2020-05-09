313fde0253b1a382b33987caa6f1b9ec248158ff
ericbsd <ericturgeon.bsd@gmail.com>
2020-05-08 23:55:03 -0300

    change the full system update to be only on kernel update

--- src/updateHandler.py
+++ src/updateHandler.py
@@ -20,11 +20,25 @@ def get_pkg_upgrade(option):
     return upgrade_verbose
 
 
+def kerenel_verstion_change():
+    pkg_update = Popen(
+        f'yes | pkg update -f',
+        shell=True,
+        stdout=PIPE,
+        close_fds=True,
+        universal_newlines=True,
+        encoding='utf-8'
+    )
+    if 'Newer FreeBSD version' in pkg_update.stdout.read():
+        return True
+    else:
+        return False
+
+
 def get_pkg_upgrade_data():
     option = ''
     system_upgrade = False
-    get_pkg_upgrade(option)
-    if 'os-generic' in get_pkg_upgrade(option):
+    if kerenel_verstion_change():
         system_upgrade = True
         option = 'f'
     update_pkg = get_pkg_upgrade(option)
